6a04a36e1be15c9e1ddaed38fe6252f2ef0a5049
jihtin <129176693+jihtin@users.noreply.github.com>
2024-12-02 18:50:09 +0530

    first commit

---
+++ src/app/Cartitmes.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Cartitmes = () => {
+  return (
+    <View>
+      <Text>Cartitmes</Text>
+    </View>
+  )
+}
+
+export default Cartitmes
